05870784f04818714aefa427c55d6440f5b85ebb
audevald <audeval.denis@gmail.com>
2020-09-23 18:26:14 +0200

    Maj Accueil

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container-fluid accueil" id="acceuil">
-        <div class="jumbotron">            
-            <h3 class="text-center">Denis AUDEVAL</h3>
-            <h1 class="text-center">DEVELOPPEUR WEB FULL STACK</h1>
+    <div class="container-fluid accueil d-flex justify-content-center align-items-center" id="acceuil">
+        <div class="text-light">            
+            <h1 class="text-center">Denis AUDEVAL</h1>
+            <h2 class="text-center">DEVELOPPEUR WEB FULL STACK</h2>
         </div>
     </div>
 </template>
@@ -17,5 +17,8 @@ export default {
 .accueil {
     padding-top: 80px;
     min-height: 100vh;
+    background-image: linear-gradient(0deg, rgba(0,0,0,0.5), rgba(0, 0, 0, 0.5)), url('../assets/img/background.jpg');
+    background-size: auto,cover;
+    background-attachment: fixed;
 }
 </style>
